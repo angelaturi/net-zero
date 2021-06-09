d96dda7a69b5aec3f6d7c9fb942db3f08d4bbeec
Mohammad Taher <zohebtaher@gmail.com>
2021-06-09 11:17:18 -0400

    working through merge conflicts

--- frontend/src/components/pledges/pledgeindex.jsx
+++ frontend/src/components/pledges/pledgeindex.jsx
@@ -11,7 +11,6 @@ class PledgeIndex extends React.Component {
     };
     this.toggleFollow = this.toggleFollow.bind(this);
   }
-    
 
   toggleFollow(pledge) {
     //add userId to pledge follow array
@@ -31,31 +30,39 @@ class PledgeIndex extends React.Component {
     this.setState({ pledges: newState.pledges });
   }
 
-    render() {
-        
-        if (this.state.pledges.length === 0) {
-            return (<div>There are no Pledges</div>)
-        } else {
-            return (
-                <div>
-                    <h2>Pledges</h2>
-                    {this.state.pledges.map(pledge => (
-                    <div className="feedItem">
-                        <p>By {pledge.user.handle}</p>
-                        <div className="pledge-title">{pledge.title}</div>
-                        <div className="pledge-description">{pledge.description}</div>
-                        <p>{pledge.follows.length} followers!</p>
-                        <p>{pledge.category}</p> 
-                        <Link className="pledge-index-link" style={{ textDecoration: 'none' }}  to={`/pledges/${pledge._id}`}>View Pledge</Link>
-                        <button className="follow-button" onClick={() => this.toggleFollow(pledge)}>
-                            { pledge.follows.includes(this.props.currentUser.id) ? "Unfollow Pledge" : "Follow Pledge" }
-
-                        </button>
-                    </div>
-                    ))}
-                </div>
-            )
-        }
+  render() {
+    if (this.state.pledges.length === 0) {
+      return <div>There are no Pledges</div>;
+    } else {
+      return (
+        <div>
+          <h2>Pledges</h2>
+          {this.state.pledges.map((pledge) => (
+            <div className="feedItem">
+              <p>By {pledge.user.handle}</p>
+              <div className="pledge-title">{pledge.title}</div>
+              <div className="pledge-description">{pledge.description}</div>
+              <p>{pledge.follows.length} followers!</p>
+              <p>{pledge.category}</p>
+              <Link
+                className="pledge-index-link"
+                style={{ textDecoration: "none" }}
+                to={`/pledges/${pledge._id}`}
+              >
+                View Pledge
+              </Link>
+              <button
+                className="follow-button"
+                onClick={() => this.toggleFollow(pledge)}
+              >
+                {pledge.follows.includes(this.props.currentUser.id)
+                  ? "Unfollow Pledge"
+                  : "Follow Pledge"}
+              </button>
+            </div>
+          ))}
+        </div>
+      );
     }
   }
 }
